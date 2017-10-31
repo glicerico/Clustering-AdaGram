dc154c3bf5839b5dffa904bc387ab7199f9efe90
Curtis Faith <suarezandres@gmail.com>
2017-10-31 10:06:12 +0800

    added visualizeEmbeddings.sh

--- visualizeEmbeddings.sh
+++ visualizeEmbeddings.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source ~/tensorflow/bin/activate
-python3 $DIR/visualizeEmbeddings.py $@
+python $DIR/visualizeEmbeddings.py $@
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "open http://localhost:6006/#embeddings in web browser"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
